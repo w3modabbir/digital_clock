8b0cf831e49bd2fd935fa62abc72315aa6b31c2e
w3modabbir <modabbirhossen.web@gmail.com>
2024-02-04 20:43:48 +0600

    js done

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,22 @@ let second = document.getElementById("second")
 
 setInterval(()=>{
     let currentTime = new Date();
-    
-    hours.innerHTML = (currentTime.getHours()<10?"0" : "") + currentTime.getHours();
-    munite.innerHTML = (currentTime.getMinutes()<10?"0" : "") + currentTime.getMinutes();
-    second.innerHTML = (currentTime.getSeconds()<10? "0" : "") + currentTime.getSeconds();
+    hours.innerHTML = currentTime.getHours();
+    munite.innerHTML = currentTime.getMinutes();
+    second.innerHTML = currentTime.getSeconds();
+    let day_night = "AM"
+    if(hours > 12){
+        hours = hours -12;
+        day_night = "PM"
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+    if(munite < 10){
+        munite = "0" + munite;
+    }
+    if(second < 10){
+        second = "0" + second;
+    }
     
 }, 1000)
